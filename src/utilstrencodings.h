0ba36c5499f433254e626cfc65bd9bd983b334f4
acostarodrigo <acosta_rodrigo@hotmail.com>
2016-09-08 20:05:53 -0300

    added new functionality in utilstrencodings to be able to parse an hex string into text. This function is used to read the op_return output of whitelist transactions.

--- src/utilstrencodings.h
+++ src/utilstrencodings.h
@@ -117,6 +117,31 @@ inline std::string HexStr(const T& vch, bool fSpaces=false)
 }
 
 /**
+ * Converts a string in Hex to text
+ */
+inline std::string StrHex(const std::string& input){
+	static const char* const lut = "0123456789ABCDEF";
+	    size_t len = input.length();
+	    //if (len & 1) throw std::invalid_argument("odd length");
+
+	    std::string output;
+	    output.reserve(len / 2);
+	    for (size_t i = 0; i < len; i += 2)
+	    {
+	        char a = input[i];
+	        const char* p = std::lower_bound(lut, lut + 16, a);
+	        //if (*p != a) throw std::invalid_argument("not a hex digit");
+
+	        char b = input[i + 1];
+	        const char* q = std::lower_bound(lut, lut + 16, b);
+	        //if (*q != b) throw std::invalid_argument("not a hex digit");
+
+	        output.push_back(((p - lut) << 4) | (q - lut));
+	    }
+	    return output;
+}
+
+/**
  * Format a paragraph of text to a fixed width, adding spaces for
  * indentation to any added line.
  */
