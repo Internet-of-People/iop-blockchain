511d43212706769225386f40cde329cc5a4860fe
acostarodrigo <acosta_rodrigo@hotmail.com>
2016-09-20 18:04:11 -0300

    partially enforce BIP34 adoption since block 0. Remove control of coinbase transactions with scriptSig with height only if white list is not activated.

--- src/main.cpp
+++ src/main.cpp
@@ -3724,16 +3724,21 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, CBlockIn
         }
     }
 
-    // Enforce block.nVersion=2 rule that the coinbase starts with serialized block height
-    // if 750 of the last 1,000 blocks are version 2 or greater (51/100 if testnet):
-    if (block.nVersion >= 2 && IsSuperMajority(2, pindexPrev, consensusParams.nMajorityEnforceBlockUpgrade, consensusParams))
-    {
-        CScript expect = CScript() << nHeight;
-        if (block.vtx[0].vin[0].scriptSig.size() < expect.size() ||
-            !std::equal(expect.begin(), expect.end(), block.vtx[0].vin[0].scriptSig.begin())) {
-            return state.DoS(100, false, REJECT_INVALID, "bad-cb-height", false, "block height mismatch in coinbase");
-        }
+
+    /* IoP beta change - we are only enforcing BIP32 with height in coinbase ScriptSig if Miner whitelist is not active */
+    if (!fIsMinerWhiteList){
+    	// Enforce block.nVersion=2 rule that the coinbase starts with serialized block height
+    	// if 750 of the last 1,000 blocks are version 2 or greater (51/100 if testnet):
+    	if (block.nVersion >= 2 && IsSuperMajority(2, pindexPrev, consensusParams.nMajorityEnforceBlockUpgrade, consensusParams))
+		{
+			CScript expect = CScript() << nHeight;
+			if (block.vtx[0].vin[0].scriptSig.size() < expect.size() ||
+				!std::equal(expect.begin(), expect.end(), block.vtx[0].vin[0].scriptSig.begin())) {
+				return state.DoS(100, false, REJECT_INVALID, "bad-cb-height", false, "block height mismatch in coinbase");
+			}
+		}
     }
+
 
     // Validation for witness commitments.
     // * We compute the witness hash (which is the hash including witnesses) of all the block's transactions, except the
