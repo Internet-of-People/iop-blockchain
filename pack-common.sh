669276eae9d2b910d60222c6c70f63b8ca1a15d5
Markus <mmatk@users.noreply.github.com>
2016-10-01 13:59:07 +0200

    Add files via upload

--- pack-common.sh
+++ pack-common.sh
@@ -7,11 +7,12 @@ sudo checkinstall \
     --pkglicense=MIT \
     --pkggroup=net \
     --pkgname=iop-blockchain \
-    --pkgversion=0.1.rc1 \
+    --pkgversion=beta-1.0.0 \
     --pkgarch=$(dpkg \
-        --print-architecture) \
+    --print-architecture) \
     $@
-    
 
+# Package Description: IoP is a digital currency for the Internet of People that enables instant payments to anyone, anywhere in the world.
+#
 # -- If library versions has to be restricted, this escaping format works:
 #--requires="libssl1.0.0 \(\>= 1.0.0\),libevent-2.0-5 \(\>= 2.0.0\),libevent-pthreads-2.0-5 \(\>= 2.0.0\),libdb5.3++ \(\>= 5.3.0\),libqrencode3 \(\>= 3.0.0\),libprotobuf9v5 \(\>= 2.6.0\),libqt5gui5 \(\>= 5.0.0\),libboost-program-options1.58.0 \(\>= 1.58.0\),libboost-thread1.58.0 \(\>= 1.58.0\),libboost-chrono1.58.0 \(\>= 1.58.0\)" \
