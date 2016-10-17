8e3c998a28fe12b40813aa7e0790e953228df5c4
acostarodrigo <acosta_rodrigo@hotmail.com>
2016-10-17 19:34:41 -0300

    added new class MinerCap. This class is used to define and control parameters related to Mining limits for miners. Logic is included to verify if the mininCap control is enabled or not, what is the actual window that the cap calculations are using, and get the valid limits for miners.

---
+++ src/minerCap.h
@@ -0,0 +1,41 @@
+/*
+ * minerCap.h
+ *
+ *  Created on: Oct 11, 2016
+ *      Author: rodrigo
+ */
+
+#ifndef MINERCAP_H_
+#define MINERCAP_H_
+
+#include "pubkey.h"
+#include <boost/filesystem/path.hpp>
+#include <tr1/unordered_map>
+
+class CMinerCap {
+private:
+	typedef std::tr1::unordered_map <std::string, uint32_t> minerCapMap;
+public:
+	CMinerCap();
+
+	// enables the miner cap
+	void enable();
+
+	// disables miner cap
+	void disable();
+
+	// returns true if enabled.
+	bool isEnabled();
+
+	// gets the total amount of active miners.
+	int getAvgBlocksPerMiner();
+
+	// calculates when the monitoring window starts
+	int getWindowStart(int currentHeight);
+
+	// we are allowing miners to mine minerMultiplier times the average blocks / miner
+	int getMinerMultiplier();
+
+};
+
+#endif /* MINERCAP_H_ */
