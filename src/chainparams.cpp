0e491a18c607df67c3413c118b877b5ae07edc62
Amon Engemann <amon.e@gmx.net>
2017-06-09 17:23:30 +0200

    added checkpoint for testnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -249,10 +249,11 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-			( 0, uint256S("0x000000006f2bb863230cda4f4fbee520314077e599a90b9c6072ea2018d7f3a3")),
-			1463452342,
-			0,
-			300
+			(   0, uint256S("0x000000006f2bb863230cda4f4fbee520314077e599a90b9c6072ea2018d7f3a3"))
+			(7800, uint256S("0x000000008c8074841b9ed5c62d61a84f659f19f716b56f774a3d9a28fec693a4")),
+      1496247512,
+			8820,
+			100
         };
 
     }
